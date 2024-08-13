eca8abf2a813f8034c2ba5d272ed5974d36814a7
Jonathan Dobson <fromjdobson@gmail.com>
2024-08-13 15:47:49 +0800

    hello world

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="applm",
+    version="0.0.1",
+    # packages=["applm"],
+    install_requires=["numpy", "mlx"],
+    author="Jonathan Dobson",
+    url="https://github.com/Jonathan-Dobson/applm",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
